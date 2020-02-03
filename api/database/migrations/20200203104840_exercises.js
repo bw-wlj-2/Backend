db2a2e1979fcc27450e707c3a500f332375d1e1b
Chase Redford <chaseredford@Chases-MacBook-Pro.local>
2020-02-03 12:23:59 -0700

    ADDED TOKEN AND EXCERCISE MIGRATION

---
+++ api/database/migrations/20200203104840_exercises.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("exercises", (exercises) => {
+    exercises.increments();
+
+    exercises.string("name").notNullable();
+
+    exercises.string("region").notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("exercises");
+};
