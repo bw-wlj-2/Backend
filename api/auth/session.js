c1dcc12ebba246317a9cab0d78af77f55ef4a62b
Chase Redford <chaseredford@Chases-MacBook-Pro.local>
2020-02-04 09:36:30 -0700

    Bugs

--- api/auth/session.js
+++ api/auth/session.js
@@ -1,7 +1,28 @@
+// module.exports = (req, res, next) => {
+//     if (req.session && req.session.loggedIn) {
+//         next();
+//     } else {
+//         res.status(401).json({ error: 'not logged in' });
+//     }
+//   };
+
+const jwt = require("jsonwebtoken");
+
 module.exports = (req, res, next) => {
-    if (req.session && req.session.loggedIn) {
+  const { authorization } = req.headers;
+
+  if (authorization) {
+    const secret = process.env.JWT_SECRET || "This is the secret";
+
+    jwt.verify(authorization, secret, function(err, decodedToken) {
+      if (err) {
+        res.status(401).json({ message: "Invalid Token" });
+      } else {
+        req.token = decodedToken;
         next();
-    } else {
-        res.status(401).json({ error: 'not logged in' });
-    }
-  };
+      }
+    });
+  } else {
+    res.status(400).json({ message: "Please login and try again" });
+  }
+};
---
+++ exercises/exercises-router.js
@@ -0,0 +1,103 @@
+const router = require("express").Router();
+const Exercises = require("./exercises-model.js");
+const restricted = require("../api/auth/session.js");
+//* GET REQUEST ==> /api/exercises ======================================
+router.get("/", restricted, (req, res) => {
+  Exercises.find()
+    .then((exercise) => {
+      res.status(200).json(exercise);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+//* =====================================================================
+
+//* GET REQUEST ==> /api/exercises/:id ======================================
+router.get("/:id", restricted, (req, res) => {
+  Exercises.findById(req.params.id)
+    .then((exercise) => {
+      res.status(200).json(exercise);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+//* =====================================================================
+
+//* GET REQUEST ==> /api/exercises/regions/:region ======================================
+router.get("/regions/:region", restricted, (req, res) => {
+  const { region } = req.params;
+  Exercises.findByRegion(region)
+    .then((exercise) => {
+      res.status(200).json(exercise);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+//* =====================================================================
+
+//* POST REQUEST ==> /api/exercises ======================================
+router.post("/", restricted, (req, res) => {
+  const newexercise = req.body;
+  if (!newexercise.name) {
+    res.status(422).json({ message: "Missing fields: name" });
+  }
+  if (!newexercise.region) {
+    res.status(422).json({ message: "Missing fields: region" });
+  }
+  Exercises.add(newexercise)
+    .then((item) => {
+      res.status(201).json(item);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+//* =====================================================================
+
+//* PUT REQUEST ==> /api/exercises/:id ======================================
+router.put("/:id", restricted, (req, res) => {
+  const { id } = req.params;
+  let changes = req.body;
+  if (!changes.name) {
+    res.status(422).json({ message: "Missing fields: name" });
+  }
+  if (!changes.region) {
+    res.status(422).json({ message: "Missing fields: region" });
+  }
+  Exercises.update(id, changes)
+    .then((updated) => {
+      if (updated) {
+        res.status(200).json({ success: true, updated });
+      } else {
+        res.status(404).json({ message: "This exercise could not be updated" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+//* =====================================================================
+
+//* DELETE REQUEST ==> /api/exercises/:id ======================================
+router.delete("/:id", restricted, (req, res) => {
+  Exercises.remove(req.params.id)
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({
+          message: "This exercise has been removed from the database",
+        });
+      } else {
+        res.status(404).json({ message: "This exercise does not exist" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+//* =====================================================================
+
+module.exports = router;
