8f58053ca58a80110ddaafb6b014c29234a762b2
dantebui <senpaicaboose@gmail.com>
2020-02-06 10:30:59 -0700

    exercise changes

--- api/models/exercises-model.js
+++ api/models/exercises-model.js
@@ -36,7 +36,7 @@ function findByRegion(region) {
 
 function findById(id) {
   return db("exercises")
-    .select("name", "region")
+    .select("name", "region", "current_pounds", "reps", "date_completed")
     .where({ id })
     .first();
 }
